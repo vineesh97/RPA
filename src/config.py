a5045418523fe912912648c2c6aa6424695976fa
ssr-inet <sathyaswaruban@inetcsc.com>
2025-03-22 15:31:13 +0530

    changes

--- src/config.py
+++ src/config.py
@@ -5,7 +5,5 @@ CONFIG = {
         "host": "192.168.1.70",
         "database": "ihub_dev",
         "port": "3307"
-    },
-    "excel_file":"D:\\Github\\RPA\\data\\Recharge-Statement.xlsx",
-    "log_file": "logs/reconciliation.log"
+    }
 }
--- src/main.py
+++ src/main.py
@@ -10,11 +10,12 @@ if __name__ == "__main__":
     end_date = input("Enter end date (YYYY-MM-DD): ")
     service_name = input("enter service name: ")
 
-    df_excel = pd.read_excel(CONFIG["excel_file"], dtype=str)
+    df_excel = pd.read_excel(f"D:\\Github\\RPA\\data\\uploading_excel\\{service_name}.xlsx", dtype=str)
+    print(df_excel)
     df_excel["DATE"] = pd.to_datetime(df_excel["DATE"], errors="coerce").dt.date
     from_date = pd.to_datetime(f"{start_date}").date()  # Change as needed
     to_date = pd.to_datetime(f"{end_date}").date() 
-    
+
     Date_check = df_excel[(df_excel["DATE"] >= from_date) & (df_excel["DATE"] <= to_date)]
 
     if Date_check.empty:
@@ -25,13 +26,13 @@ if __name__ == "__main__":
     #calling the function module
         result = run_Reconciliation(start_date, end_date,service_name,df_excel)
 
-output_file = f"D:\\Github\\RPA\\data\\{service_name}.xlsx"
+output_file = f"D:\\Github\\RPA\\data\\exported_excel\\{service_name}.xlsx"
 
 # Use ExcelWriter to write multiple sheets in the same Excel file
 with pd.ExcelWriter(output_file, engine="xlsxwriter") as writer:
         result["mismatched"].to_excel(writer, sheet_name="Mismatched Data", index=False)
         result["not_in_Portal"].to_excel(writer, sheet_name="not_in_Portal", index=False)
-        result["not_in_vendor"].to_excel(writer, sheet_name="ot_in_vendor", index=False)
+        result["not_in_vendor"].to_excel(writer, sheet_name="not_in_vendor", index=False)
         result["VENDOR_SUCCESS_IHUB_INPROGRESS"].to_excel(writer, sheet_name="Vendor Success IHUB InProgress", index=False)
         result["VENDOR_SUCCESS_IHUB_FAILED"].to_excel(writer, sheet_name="Vendor Success IHUB Failed", index=False)
 
