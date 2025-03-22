e939cdbb416fe70ad4c2c5474af605bd41ef0c5d
ssr-inet <sathyaswaruban@inetcsc.com>
2025-03-22 12:12:51 +0530

    validating the gn date range in excel

--- src/main.py
+++ src/main.py
@@ -1,26 +1,30 @@
-from reconciliation import run_reconciliation
+from reconciliation import run_Reconciliation
 import pandas as pd
 from logger_config import logger
+from config import CONFIG
+import sys  
 
 if __name__ == "__main__":
     #Initial execution Getting from and to date from user
     start_date = input("Enter start date (YYYY-MM-DD): ")
     end_date = input("Enter end date (YYYY-MM-DD): ")
     service_name = input("enter service name: ")
-   
+
+    df_excel = pd.read_excel(CONFIG["excel_file"], dtype=str)
+    df_excel["DATE"] = pd.to_datetime(df_excel["DATE"], errors="coerce").dt.date
+    from_date = pd.to_datetime(f"{start_date}").date()  # Change as needed
+    to_date = pd.to_datetime(f"{end_date}").date() 
+    
+    Date_check = df_excel[(df_excel["DATE"] >= from_date) & (df_excel["DATE"] <= to_date)]
+
+    if Date_check.empty:
+        print("⚠️ No records found within the given date range!.Kindly the Date Entered")
+        sys.exit(0)
+    else:
+        print("✅ Records found within the date range:")
     #calling the function module
-    logger.info("-----------------------------------------------------------------------------------------------------")
-    result = run_reconciliation(start_date, end_date,service_name)
+        result = run_Reconciliation(start_date, end_date,service_name,df_excel)
 
-    #Printing values for our reference
-    #print("Not in Excel:", result["not_in_excel"])
-    #print("Not in Server:", result["not_in_server"])
-    #print("Mismatched:", result["mismatched"])
-
-    #Exporting Seperated data to excel
-    #result["mismatched"].to_excel("D:\\Github\\RPA\\data\\Mismatched_values.xlsx",index=False)
-    #result["not_in_server"].to_excel("D:\\GitHub\\RPA\\rpa_reconciliation\\rpa_reconciliation\\data\\NotinHub_values.xlsx",index=False)
-    #result["not_in_excel"].to_excel("D:\\GitHub\\RPA\\rpa_reconciliation\\rpa_reconciliation\\data\\NotinVendor_values.xlsx",index=False)
 output_file = f"D:\\Github\\RPA\\data\\{service_name}.xlsx"
 
 # Use ExcelWriter to write multiple sheets in the same Excel file
@@ -34,4 +38,3 @@ with pd.ExcelWriter(output_file, engine="xlsxwriter") as writer:
 print(f"Report successfully saved to {output_file}")
 logger.info("Report successfully Exported")
 logger.info("-----------------------------------------------------------------------------------------------------")
-
--- src/reconciliation.py
+++ src/reconciliation.py
@@ -4,17 +4,19 @@ from config import CONFIG
 from logger_config import logger
 engine = get_db_connection()
 
-def run_reconciliation(start_date,end_date,service_name):
-    logger.info("---------Entering Reconciliation---------")
-
+def run_Reconciliation(start_date,end_date,service_name,df_excel):
+    logger.info("-----------------------------------------------------------------------------------------------------")
+    logger.info(f"---------Entering Reconciliation for {service_name} Service---------")
     start_date=start_date
     end_date=end_date
+
     if service_name=='recharge':
-        result = recharge(start_date,end_date)
+        result = recharge_Service(start_date,end_date,df_excel)
     return result
 
-def filtering_data (df_db,df_excel):
-    logger.info("---Filteration Starts:")
+
+def filtering_Data (df_db,df_excel):
+    logger.info("---Filteration Starts---")
 
     columns_to_update = ["HUB_Master_status", "MasterSubTrans_status","Tenant_Status"]
     df_db[columns_to_update] = df_db[columns_to_update].apply(lambda x: x.map({0:"inititated",1: "success", 2: "failed", 3: "In progress",4:"Partial success"}).fillna(x))
@@ -31,24 +33,25 @@ def filtering_data (df_db,df_excel):
     
     #Seperating data matching in both but having diff status value 
     mismatched = matched[matched["Recharge_status"].str.lower() != matched["STATUS"].str.lower()]
-   # return 0
+
    #VENDOR_SUCCESS_IHUB_INPROGRESS
     VENDOR_SUCCESS_IHUB_INPROGRESS = mismatched[
             (mismatched['STATUS'] == 'Success') &  # STATUS in df_recharge_excel is Success
             (mismatched['Recharge_status'] == "success") &  # Recharge_status in df1 is Success
             (mismatched['HUB_Master_status'] == "In progress" )  # Mst_status in df1 is NOT Success
-    ]
+        ]
    #VENDOR_SUCCESS_IHUB_FAILED___
     VENDOR_SUCCESS_IHUB_FAILED = mismatched[
             (mismatched['STATUS'] == 'Success') &  # STATUS in df_recharge_excel is Success
             (mismatched['Recharge_status'] == "success") &  # Recharge_status in df1 is Success
             (mismatched['HUB_Master_status'] == "failed" )  # Mst_status in df1 is NOT Success
-    ]
-    logger.info("---Filteration Ends:")
+        ]
+    logger.info("---Filteration Ends---")
     return {"not_in_vendor": not_in_vendor, "not_in_Portal": not_in_Portal.head(100), "mismatched": mismatched,"VENDOR_SUCCESS_IHUB_INPROGRESS":VENDOR_SUCCESS_IHUB_INPROGRESS ,"VENDOR_SUCCESS_IHUB_FAILED":VENDOR_SUCCESS_IHUB_FAILED}  
 
-def recharge(start_date, end_date):
-    logger.info("--Service mapping---")
+#Recharge service function
+def recharge_Service(start_date, end_date,df_excel):
+    logger.info("---Service mapping---")
     query = f'''
         select mt2.TransactionRefNum ,sn.requestID as vendor_reference ,mt.TransactionStatus  as Tenant_Status,mt2.TransactionStatus  as HUB_Master_status, 
         mst.TransactionStatus as MasterSubTrans_status,sn.rechargeStatus  as  Recharge_status
@@ -64,9 +67,32 @@ def recharge(start_date, end_date):
     
    #Reading data from both Server and Excel 
     df_db = pd.read_sql(query, con=engine)
+
+    #replacing the enums to its corresponding status values
+    df_db["Recharge_status"]=df_db["Recharge_status"].apply(lambda x: "success" if x == 1 else "pending" if x == 2 else "failed" if x == 3 else "instant failed" if x==4 else x )
+    result=filtering_Data(df_db,df_excel)
+    return result
+
+def aeps_Service(start_date, end_date):
+    logger.info("---Service mapping---")
+    query = f'''
+       select mt2.TransactionRefNum ,pat.requestID as vendor_reference ,mt.TransactionStatus  as Tenant_Status,mt2.TransactionStatus  as HUB_Master_status, 
+        mst.TransactionStatus as MasterSubTrans_status,pat.TransStatus  as  Aeps_status
+        from iHubTenantPortal_dev.MasterTransaction mt 
+        left join ihub_dev.MasterTransaction mt2 
+        on mt.Id =mt2.TenantMasterTransactionId 
+        left join ihub_dev.MasterSubTransaction mst 
+        on mst.MasterTransactionId  =mt2.Id
+        left join ihub_dev.PsAepsTransaction pat  
+        on pat.MasterSubTransactionId =mst.Id 
+        where mt2.TenantDetailId = 1 and 
+        DATE(sn.CreationTs) BETWEEN '{start_date}' AND '{end_date}' '''
+    
+   #Reading data from both Server and Excel 
+    df_db = pd.read_sql(query, con=engine)
     df_excel = pd.read_excel(CONFIG["excel_file"], dtype=str)
 
     #replacing the enums to its corresponding status values
     df_db["Recharge_status"]=df_db["Recharge_status"].apply(lambda x: "success" if x == 1 else "pending" if x == 2 else "failed" if x == 3 else "instant failed" if x==4 else x )
-    result=filtering_data(df_db,df_excel)
+    result=filtering_Data(df_db,df_excel)
     return result
